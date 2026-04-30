e756cc455afbad5303430711df28b94ba028575f
nhuyeumeo2811-del <nhuyeumeo2811@gmail.com>
2026-04-30 16:32:47 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import DetailProduct from './components/Products/DetailProduct';
-import ProductList from './components/Products/ProductList';
-import ProductCard from './components/Products/ProductCard';
+import ProductList from "./components/Products/ProductList";
+import ProductCard from "./components/Products/ProductCard";
+import Cart from "./components/Pages/Cart";
 
 function App() {
   const location = useLocation();
@@ -15,9 +16,9 @@ function App() {
         <Route path="/" element={<ProductList />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/product/:id" element={<DetailProduct />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
-
     </>
   );
 }
